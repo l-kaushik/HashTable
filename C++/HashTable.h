711acf7e9cb714081f1277d0e6e4549b264edaef
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-12 13:57:44 +0530

    Added simple hashing 1.Added a hashing helper function which will get called based on types.

--- C++/HashTable.h
+++ C++/HashTable.h
@@ -2,6 +2,7 @@
 #define HASH_TABLE
 
 #include <iostream>
+#include <type_traits>
 #include <vector>
 
 #include "Node.h"
@@ -11,6 +12,10 @@ class HashTable
 {
 private:
     std::vector<Node<U> *> array_of_buckets;
+    
+    //helper functions for insertion
+    std::size_t hashingHelper(T key, std::true_type);
+    std::size_t hashingHelper(T key, std::false_type);
 
 public:
     // constructors
@@ -18,14 +23,14 @@ public:
     HashTable(int size);
     HashTable(size_t size) : array_of_buckets(size) {}
 
-    //insertion
-    void insert(T key,U value);
+    // insertion
+    void insert(T key, U value);
+ 
+    // core functions
+    std::size_t hashing(T key); // return index;
 
-    //core functions
-    std::size_t hashing(T key); //return index
-
-    //getters
-    U getValue(T key);  //@return value
+    // getters
+    U getValue(T key); //@return value
 
     // Utility
     auto size() { return (array_of_buckets.size()); }
@@ -36,7 +41,7 @@ public:
 
 // constructor
 template <typename T, typename U>
-HashTable<T,U>::HashTable(int size) : array_of_buckets(static_cast<size_t>(size < 1 ? -1 * size : size))
+HashTable<T, U>::HashTable(int size) : array_of_buckets(static_cast<size_t>(size < 1 ? -1 * size : size))
 {
     if (size < 1)
         std::cerr << "Warning: "
@@ -45,37 +50,44 @@ HashTable<T,U>::HashTable(int size) : array_of_buckets(static_cast<size_t>(size 
                   << -1 * size << '\n';
 }
 
-//insertion
+// insertion
 template <typename T, typename U>
 void HashTable<T, U>::insert(T key, U value)
 {
-    //function to get index from key
+    // function to get index from key
     array_of_buckets[hashing(key)] = new Node<U>(value); // try to use smart pointers
 
-    //implement [] operator to store value
+    // implement [] operator to store value
 }
 
-//core functions
-
+// core functions
 template <typename T, typename U>
 std::size_t HashTable<T, U>::hashing(T key)
 {
-    //TODO: int and int64_t both are different, so implement accordingly
-    if(typeid(T) == typeid(int))    //! imagine T as int for now
-    {
-        // index
-        return static_cast<std::size_t>(key) % size();
-    }
-    return 1;
+    return hashingHelper(key, std::is_arithmetic<T>{});
 }
 
-//getters
+//helper functions
+template <typename T, typename U>
+std::size_t HashTable<T, U>::hashingHelper(T key, std::true_type) {
+    return static_cast<std::size_t>(key) % size();
+}
+
+template <typename T, typename U>
+std::size_t HashTable<T, U>::hashingHelper(T key, std::false_type) {
+    std::size_t alpha_count{};
+    for(auto &i: key)
+        alpha_count += static_cast<std::size_t>(i);
+
+    return alpha_count % size();
+}
+
+// getters
 template <typename T, typename U>
 U HashTable<T, U>::getValue(T key)
 {
-    //get index from hashing function
+    // get index from hashing function
     return array_of_buckets[hashing(key)]->getValue();
 }
-
 
 #endif
